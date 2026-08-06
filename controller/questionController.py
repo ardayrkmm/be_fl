767fdb1e0335235e65dc8f83ccf33c037deda8ca
ardayrkmm <ardyrkm23@gmail.com>
2026-08-07 05:47:13 +0700

    Fix duration options to only have <2 and >2

--- controller/questionController.py
+++ controller/questionController.py
@@ -25,12 +25,11 @@ def update_durasi_db():
             for opt in q.options:
                 db.session.delete(opt)
             
-            # Buat option baru
+            # Buat option baru (Hanya 2 opsi sesuai permintaan)
             opt1 = QuestionOption(id=str(uuid.uuid4())[:8], key="<2", label="Kurang dari 2 minggu", nilai=1, question_id=q.id)
-            opt2 = QuestionOption(id=str(uuid.uuid4())[:8], key="2-4", label="2-4 minggu", nilai=4, question_id=q.id)
-            opt3 = QuestionOption(id=str(uuid.uuid4())[:8], key=">4", label="Lebih dari 4 minggu", nilai=7, question_id=q.id)
+            opt2 = QuestionOption(id=str(uuid.uuid4())[:8], key=">2", label="Lebih dari 2 minggu", nilai=4, question_id=q.id)
             
-            db.session.add_all([opt1, opt2, opt3])
+            db.session.add_all([opt1, opt2])
             
         db.session.commit()
         return jsonify({"success": True, "message": "Database berhasil diupdate untuk durasi nyeri!"}), 200
@@ -539,3 +538,11 @@ def submit_final_program_assessment():
             "success": False,
             "error": str(e)
         }), 500
+@question_bp.route('/debug-questions', methods=['GET'])
+def debug_questions():
+    qs = Question.query.all()
+    res = []
+    for q in qs:
+        res.append({'id': q.id, 'title': q.title, 'category': q.category, 'id_bagian': q.id_bagian})
+    return jsonify(res)
+
--- update_db.py
+++ update_db.py
@@ -24,10 +24,9 @@ with app.app_context():
         
         # Buat option baru
         opt1 = QuestionOption(id=str(uuid.uuid4())[:8], key="<2", label="Kurang dari 2 minggu", nilai=1, question_id=q.id)
-        opt2 = QuestionOption(id=str(uuid.uuid4())[:8], key="2-4", label="2-4 minggu", nilai=4, question_id=q.id)
-        opt3 = QuestionOption(id=str(uuid.uuid4())[:8], key=">4", label="Lebih dari 4 minggu", nilai=7, question_id=q.id)
+        opt2 = QuestionOption(id=str(uuid.uuid4())[:8], key=">2", label="Lebih dari 2 minggu", nilai=4, question_id=q.id)
         
-        db.session.add_all([opt1, opt2, opt3])
+        db.session.add_all([opt1, opt2])
         print("Updated options for question:", q.title)
         
     db.session.commit()
